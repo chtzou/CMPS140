0f1d1d89f77f8c725a74e2a3613b3dc7be317b35
Hannah Ekman <>
2019-02-03 14:03:16 -0800

    fix

--- negativeWordCount.py
+++ negativeWordCount.py
@@ -6,7 +6,7 @@ filename='badQuestions.csv'
 f = open(filename)
 csv_f = csv.reader(f)
 
-ignore=['if', 'the', 'then', 'what', 'a', 'why', 'who', 'them', 'she', 'we', 'they', 'and', 'be', 'to', 'of', 'in', 'that', 'have', 'I', 'it', 'for', 'not', 'on', 'with', 'he', 'as', 'you', 'do', 'at', 'this', 'but', 'his', 'by', 'from', 'say', 'her', 'or', 'will', 'an', 'my', 'one', 'all', 'would', 'there', 'their', 'what', 'so', 'up', 'out', 'about', 'get', 'which', 'go', 'when', 'me', 'make', 'can', 'like', 'time', 'no', 'just', 'him', 'know', 'take', 'person', 'into', 'year', 'your', 'good', 'some', 'could', 'them', 'see', 'other', 'than', 'then', 'now', 'look', 'only', 'come', 'its', 'over', "it's", 'think', 'also', 'back', 'after', 'use', 'two', 'how', 'our', 'work', 'first', 'well', 'way', 'even', 'new', 'want', 'because', 'any', 'these', 'give', 'day', 'most', 'us', ',', '.', '?', '!', ':']
+ignore=['if', 'the', 'then', 'what', 'a', 'why', 'who', 'them', 'she', 'we', 'they', 'and', 'be', 'to', 'of', 'in', 'that', 'have', 'i', 'it', 'for', 'not', 'on', 'with', 'he', 'as', 'you', 'do', 'at', 'this', 'but', 'his', 'by', 'from', 'say', 'her', 'or', 'will', 'an', 'my', 'one', 'all', 'would', 'there', 'their', 'what', 'so', 'up', 'out', 'about', 'get', 'which', 'go', 'when', 'me', 'make', 'can', 'like', 'time', 'no', 'just', 'him', 'know', 'take', 'person', 'into', 'year', 'your', 'good', 'some', 'could', 'them', 'see', 'other', 'than', 'then', 'now', 'look', 'only', 'come', 'its', 'over', "it's", 'think', 'also', 'back', 'after', 'use', 'two', 'how', 'our', 'work', 'first', 'well', 'way', 'even', 'new', 'want', 'because', 'any', 'these', 'give', 'day', 'most', 'us', ',', '.', '?', '!', ':']
 
 wordCount = {}
 
